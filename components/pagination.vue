af5f3c397cc95a2911cf840700671337dac4477c
mod728 <vinnybeach712@yahoo.co.jp>
2022-11-08 10:58:29 +0100

    Code for Chapter 5

---
+++ components/pagination.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2 class="paginationWrapper">
+        <a v-for="(paginationLink, index) in paginationLinks" :href="paginationLink" :key="index">
+            {{ index + 1 }}
+        </a>
+    </h2>
+</template>
+
+<script setup>
+const props = defineProps({
+    numberPages: Number,
+})
+
+const paginationLinks = Array.from({ length: props.numberPages }, (_, i) => i === 0 ? `/blog` : `/blog/page/${i + 1}`)
+</script>
