2fee51ca5f4db5e66a6a458c795f4fc26fe65eb1
mod728 <vinnybeach712@yahoo.co.jp>
2022-11-08 10:37:57 +0100

    Code for Chapter 3

---
+++ pages/blog/[id].vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="hero">
+            <img :src="data.image" alt="blog-image">
+        </div>
+        <div class="wrapper">  
+            <div class="container">               
+                <h1>{{ data.title }}</h1>
+                <p>{{ data.date }}</p> 
+                <ContentDoc />
+            </div> 
+        </div>
+    </div> 
+</template>
+
+<script setup>
+const { data } = await useAsyncData(useRoute().path, () => 
+    queryContent(useRoute().path).findOne()
+)
+</script>
